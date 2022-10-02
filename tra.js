aedc3e39cddec8413ad9e1187ba594f7d3515574
aliiyuu <112369864+aliiyuu@users.noreply.github.com>
2022-10-02 15:18:10 -0700

    Add files via upload

---
+++ tra.js
@@ -0,0 +1,180 @@
+var total = 0;
+
+function reset(){
+    total = 0;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+    document.getElementById("cart-contents").innerHTML = "Welcome to your cart! Items you add will appear here." + "<br>";
+}
+
+function addYinDragon(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("yin-dragon").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addMountainFlower(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("mountain-flower").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addThaiSensation(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("thai-sensation").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addStrawberryTropic(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("strawberry-tropic").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addEndearingKeira(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("endearing-keira").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addWinterland(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("winterland").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addTeaHowler(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("tea-howler").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addMatchaLatte(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("matcha-latte").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addFruitySamurai(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("fruity-samurai").innerHTML + "<br>";
+}
+
+function addTropicalTwister(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("tropical-twister").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addChilledBlossom(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("chilled-blossom").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addZestfulAlex(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("zestful-alex").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addSweetKendra(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("sweet-kendra").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addDelightfulSophia(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("delightful-sophia").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addMangoSunrise(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("mango-sunrise").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addWintermelonBreeze(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("winter-melon-breeze").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addOolongMacchiato(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("oolong-tea-macchiato").innerHTML + "<br>";
+    total += 5.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addJasmineMacchiato(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("jasmine-tea-macchiato").innerHTML + "<br>";
+    total += 5.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addJasmineTea(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("jasmine-tea").innerHTML + "<br>";
+    total += 5.00;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addOolongTea(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("oolong-tea").innerHTML + "<br>";
+    total += 5.00;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addJasmineSignature(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("jasmine-signature").innerHTML + "<br>";
+    total += 5.00;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addOolongSignature(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("oolong-signature").innerHTML + "<br>";
+    total += 5.00;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addLemonyDelight(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("lemony-delight").innerHTML + "<br>";
+    total += 5.00;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addPassionInfusion(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("passion-infusion").innerHTML + "<br>";
+    total += 5.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addStrawberrySmoothie(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("strawberry-smoothie").innerHTML + "<br>";
+    total += 6.50;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addPopcornChicken(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("popcorn-chicken").innerHTML + "<br>";
+    total += 6.75;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+function addWaffleFries(){
+    document.getElementById("cart-contents").innerHTML += "•" + "&nbsp;" + document.getElementById("waffle-fries").innerHTML + "<br>";
+    total += 6.75;
+    document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
